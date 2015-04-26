4a9209003f2b78802741399f3afaf06113741aae
marco <marco.piovan@gmail.com>
2015-04-26 11:01:40 +0200

    cqi

--- src/main/java/cfairtest/dao/TradeMessageDao.java
+++ src/main/java/cfairtest/dao/TradeMessageDao.java
@@ -1,7 +1,10 @@
 package cfairtest.dao;
 
+import javax.ejb.Stateless;
+
 import cfairtest.entity.TradeMessage;
 
+@Stateless
 public class TradeMessageDao extends GenericDao<TradeMessage>{
 
 	public TradeMessageDao(){
--- src/test/java/cfairtest/TradeResourceIT.java
+++ src/test/java/cfairtest/TradeResourceIT.java
@@ -10,7 +10,6 @@ import javax.ws.rs.core.Response;
 import org.junit.Before;
 import org.junit.Test;
 
-import cfairtest.entity.TradeMessage;
 
 public class TradeResourceIT {
 
