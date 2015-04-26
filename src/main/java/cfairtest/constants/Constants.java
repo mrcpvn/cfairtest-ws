3fc68aef4ee434ed3f771684478f100df2ec5c81
marco <marco.piovan@gmail.com>
2015-04-26 12:14:55 +0200

    date format

--- src/main/java/cfairtest/constants/Constants.java
+++ src/main/java/cfairtest/constants/Constants.java
@@ -2,7 +2,7 @@ package cfairtest.constants;
 
 public final class Constants {
 
-	public static final String DATE_FORMAT = "dd-MM-YY HH:mm:ss"; //24-JAN-15 10:27:44
+	public static final String DATE_FORMAT = "dd-MMM-yy HH:mm:ss"; //24-JAN-15 10:27:44
 	
 	private Constants(){}
 }
